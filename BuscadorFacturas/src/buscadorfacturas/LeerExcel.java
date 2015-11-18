4a2d9d568b1b6d3a3cf36592f1c20fb9cf706c5b
ZeteESP <zete0@hotmail.com>
2015-11-18 18:38:42 +0100

    Primer intento añadir Clase para leer Excel y abierta clase FileRename

---
+++ BuscadorFacturas/src/buscadorfacturas/LeerExcel.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscadorfacturas;
+
+import java.io.File;	
+import java.io.FileInputStream;
+import java.io.IOException;	
+import java.util.Iterator;
+	
+import org.apache.poi.ss.usermodel.Cell;	
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+public class LeerExcel {
+    
+    FileInputStream file = new FileInputStream(new File(excelRename));
+	
+	// Crear el objeto que tendra el libro de Excel
+	
+	XSSFWorkbook workbook = new XSSFWorkbook(file);
+ 
+        /*
+	
+	 * Obtenemos la primera pestaña a la que se quiera procesar indicando el indice.
+	
+	 * Una vez obtenida la hoja excel con las filas que se quieren leer obtenemos el iterator
+	
+	 * que nos permite recorrer cada una de las filas que contiene.
+	
+	 */
+	
+	XSSFSheet sheet = workbook.getSheetAt(0);
+	
+	Iterator<Row> rowIterator = sheet.iterator();
+	
+ 
+	
+	Row row;
+	
+	// Recorremos todas las filas para mostrar el contenido de cada celda
+	
+	while (rowIterator.hasNext()){
+	
+	    row = rowIterator.next();
+	
+ 
+	
+	    // Obtenemos el iterator que permite recorres todas las celdas de una fila
+	
+	    Iterator<Cell> cellIterator = row.cellIterator();
+	
+	    Cell celda;
+	
+ 
+	
+	    while (cellIterator.hasNext()){
+	
+		celda = cellIterator.next();
+	
+ 
+	
+		// Dependiendo del formato de la celda el valor se debe mostrar como String, Fecha, boolean, entero...
+	
+		switch(celda.getCellType()) {
+	
+		case Cell.CELL_TYPE_NUMERIC:
+	
+		    if( DateUtil.isCellDateFormatted(celda) ){
+	
+		       System.out.println(celda.getDateCellValue());
+	
+		    }else{
+	
+		       System.out.println(celda.getNumericCellValue());
+	
+		    }
+	
+		    break;
+	
+		case Cell.CELL_TYPE_STRING:
+	
+		    System.out.println(celda.getStringCellValue());
+	
+		    break;
+	
+		case Cell.CELL_TYPE_BOOLEAN:
+	
+		    System.out.println(celda.getBooleanCellValue());
+	
+		    break;
+	
+		}
+	
+	    }
+	
+	}
+	
+ 
+	
+	// cerramos el libro excel
+	
+	workbook.close();
+	
+    }
+	
+ 
+	
+}
+        
+        
+}
